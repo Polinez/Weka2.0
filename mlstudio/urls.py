ca2cabb2822bb45bc344703d2f1e77c8810676af
Sebastian <sebastianwandzel@yahoo.pl>
2025-10-10 16:14:39 +0200

    feat: working menu nav

--- mlstudio/urls.py
+++ mlstudio/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name = 'mlstudio'
 urlpatterns = [
-    path("<int:dataset_id>/", views.studio, name="studio"),
+    path("", views.studio, name="studio"),
+    path("select/<int:dataset_id>/", views.select_dataset, name="select_dataset"),
+    path("preprocess/", views.preprocess, name="preprocess"),
+    path("models/", views.models, name="models"),
+    path("run/", views.run_model, name="run_model"),
+    path("visualize/", views.visualize, name="visualize"),
 ]
--- mlstudio/views.py
+++ mlstudio/views.py
@@ -1,11 +1,55 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 from loadData.models import Dataset
 import pandas as pd
 import io
 
-def studio(request, dataset_id):
+def load_data_from_sesion(request):
+    dataset_id = request.session.get('dataset_id')  # Retrieve dataset_id from session
+
+    if not dataset_id:
+        return render(request, "loadData:loadData.html", {"error": "No dataset selected."})
+
     dataset = get_object_or_404(Dataset, id=dataset_id, user=request.user)
+    return dataset
+
+
+@login_required
+@require_POST
+def select_dataset(request, dataset_id):
+    dataset = get_object_or_404(Dataset, id=dataset_id, user=request.user)
+    request.session['dataset_id'] = dataset.id  # Store dataset_id in session
+    return redirect("mlstudio:studio")
+
+
+def studio(request):
+    dataset = load_data_from_sesion(request)
+
     df = pd.read_csv(io.StringIO(dataset.data))  # konwersja do DataFrame
 
     preview = df.head().to_html()
     return render(request, "explore.html", {"dataset": dataset, "preview": preview})
+
+
+def preprocess(request):
+    dataset = load_data_from_sesion(request)
+
+    return render(request, "preprocess.html", {"dataset": dataset})
+
+def models(request):
+    dataset = load_data_from_sesion(request)
+
+    return render(request, "models.html", {"dataset": dataset})
+
+def run_model(request):
+    dataset = load_data_from_sesion(request)
+
+    return render(request, "run.html", {"dataset": dataset})
+
+def visualize(request):
+    dataset = load_data_from_sesion(request)
+
+    return render(request, "visualize.html", {"dataset": dataset})
+
+
