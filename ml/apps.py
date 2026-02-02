4951e32408a4917caaecf914c58a4b47f3251de9
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2026-02-02 16:09:28 +0100

    fix: deleting files after archive

--- ml/apps.py
+++ ml/apps.py
@@ -5,6 +5,7 @@ class MlConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ml'
     verbose_name = 'Machine Learning'
+    default = True
 
     def ready(self):
         import ml.signals
--- preprocessing/apps.py
+++ preprocessing/apps.py
@@ -5,6 +5,7 @@ class PreprocessingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'preprocessing'
     verbose_name = 'Preprocessing'
+    default = True
 
     def ready(self):
         import preprocessing.signals
