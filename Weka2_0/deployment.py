a5a2ccebe65a87ed26066af0d62f64b9e6460d3f
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2026-02-04 18:18:18 +0100

    Update deployment.py

--- Weka2_0/deployment.py
+++ Weka2_0/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY=os.environ['SECRET']
+SECRET=os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 CSRF_TRUSTED_ORIGINS = [f"https://{os.environ['WEBSITE_HOSTNAME']}"]
 DEBUG = False
